a5abbf1507fe9b7bd802354a948b255ca362cf36
breyshaw <dawsonbrey@gmail.com>
2022-02-23 15:38:48 -0500

    add solution

--- java_fizzbuzz/src/main/java/com/breyshaw/App.java
+++ java_fizzbuzz/src/main/java/com/breyshaw/App.java
@@ -2,21 +2,26 @@ package com.breyshaw;
 
 import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Number:");
         int number = scanner.nextInt();
-        System.out.println(number);
+        if (number % 5 == 0 && number % 3 == 0) {
+            System.out.println("Fizzbuzz");
+        } else if (number % 5 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
+        }
     }
 }
 
-
-//Have the user enter a number
-//If the number is divisible by 5, we get fizz
-//If the number is divisible by 10, we get fizz
-//If the number is divisible by 3, we get buzz
-//If the number is divisible by 5 and 3 we get fizzbuzz
-//If it is divisible by none, it returns the number entered
+// Have the user enter a number
+// If the number is divisible by 5, we get fizz
+// If the number is divisible by 10, we get fizz
+// If the number is divisible by 3, we get buzz
+// If the number is divisible by 5 and 3 we get fizzbuzz
+// If it is divisible by none, it returns the number entered
