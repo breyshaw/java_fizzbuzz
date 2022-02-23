831a7a634f40a5261dfe884f364a9858ad141116
breyshaw <dawsonbrey@gmail.com>
2022-02-23 15:17:19 -0500

    add psuedocode

--- java_fizzbuzz/src/main/java/com/breyshaw/App.java
+++ java_fizzbuzz/src/main/java/com/breyshaw/App.java
@@ -7,3 +7,11 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+
+
+//Have the user enter a number
+//If the number is divisible by 5, we get fizz
+//If the number is divisible by 10, we get fizz
+//If the number is divisible by 3, we get buzz
+//If the number is divisible by 5 and 3 we get fizzbuzz
+//If it is divisible by none, it returns the number entered
